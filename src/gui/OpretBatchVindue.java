376bf1c377c27de56f3911461599784d04f4eda1
Jonas-Petersen <jonasvm6@hotmail.com>
2025-05-07 11:30:27 +0200

    OpretBatch og gui

--- src/gui/OpretBatchVindue.java
+++ src/gui/OpretBatchVindue.java
@@ -1,10 +1,23 @@
 package gui;
 
+import application.controller.Controller;
+import application.model.Fad;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import storage.Storage;
 
 public class OpretBatchVindue extends Stage {
+    private ListView<Fad> fadListView = new ListView<>();
+    private TextField fadNavnTextField = new TextField();
+    private TextField batchNavnTxtField = new TextField();
+    private TextField batchNummerTxtField = new TextField();
+    private TextField fortyndelseTxtField = new TextField();
+
     public OpretBatchVindue(String title) {
 
         this.setResizable(false);
@@ -18,8 +31,81 @@ public class OpretBatchVindue extends Stage {
     }
 
     private void initContent(GridPane pane) {
+        pane.setPadding(new Insets(20));
+        pane.setPrefHeight(450);
+        pane.setPrefWidth(500);
+        pane.setVgap(10);
+        pane.setHgap(30);
+
+        Label vælgFadLabel = new Label("Vælg et fad der ønskes tappet:");
+        fadListView.getItems().setAll(Storage.getFade());
+        fadListView.setPrefHeight(300);
+
+        fadListView.setOnMouseClicked(event -> {
+            fadNavnTextField.setText(String.valueOf(fadListView.getSelectionModel().getSelectedItem()));
+        });
+
+        Label fadNavnLabel = new Label("Fad: ");
+        fadNavnTextField.setPromptText("Fad navn: ");
+        fadNavnTextField.setEditable(false);
 
 
+        Label batchNavn = new Label("Batch navn: ");
+        batchNavnTxtField.setPromptText("Indtast navn på batch");
+
+        Label batchNummer = new Label("Batch Nummer: ");
+        batchNummerTxtField.setPromptText("Indtast nummer på batch");
+
+        Button minusBtn = new Button("-");
+        minusBtn.setOnMouseClicked(event -> {
+            if (Integer.parseInt(fortyndelseTxtField.getText()) - 1 >= 0) {
+                fortyndelseTxtField.setText(String.valueOf(Integer.parseInt(fortyndelseTxtField.getText()) - 1));
+            }
+        });
+        fortyndelseTxtField.setPrefWidth(40);
+        Button plusBtn = new Button("+");
+        plusBtn.setOnMouseClicked(event -> fortyndelseTxtField.setText(String.valueOf(Integer.parseInt(fortyndelseTxtField.getText()) + 1)));
+
+
+        Label fortyndelseLabel = new Label("Fortyndelse i liter: ");
+
+        Button lavTapningBtn = new Button("Foretag tapning");
+        lavTapningBtn.setOnMouseClicked(event -> {
+
+            try {
+                Controller.opretBatch(fadListView.getSelectionModel().getSelectedItem(), batchNavnTxtField.getText(), Integer.parseInt(batchNummerTxtField.getText()), Double.parseDouble(fortyndelseTxtField.getText()));
+                Alert succesAlert = new Alert(Alert.AlertType.CONFIRMATION);
+                succesAlert.setTitle("Batch oprettet!");
+                succesAlert.setHeaderText("Batch er er nu oprettet");
+                succesAlert.show();
+                this.close();
+            } catch (NumberFormatException e) {
+                if (fadListView.getSelectionModel().isEmpty() || batchNavnTxtField.getText().isEmpty() || batchNummerTxtField.getText().isEmpty() || fortyndelseTxtField.getText().isEmpty()) {
+                    Alert fejlAlert = new Alert(Alert.AlertType.ERROR);
+                    fejlAlert.setTitle("Invalid information");
+                    fejlAlert.setHeaderText("Alle felter skal udfyldes");
+                    fejlAlert.show();
+                }
+                else {
+                    Alert fejlAlert = new Alert(Alert.AlertType.ERROR);
+                    fejlAlert.setTitle("Invalid information");
+                    fejlAlert.setHeaderText("Ugyldig information indtastet");
+                    fejlAlert.show();
+                }
+            }
+        });
+
+        HBox fortyndelseBox = new HBox(minusBtn, fortyndelseTxtField, plusBtn);
+        pane.add(fortyndelseBox, 2, 1);
+        VBox listBox = new VBox(vælgFadLabel, fadListView);
+        listBox.setSpacing(5);
+        pane.add(listBox, 0, 0);
+
+
+        VBox tapningVBox = new VBox(fortyndelseLabel, fortyndelseBox, lavTapningBtn);
+        VBox informationBox = new VBox(fadNavnLabel, fadNavnTextField, batchNavn, batchNavnTxtField, batchNummer, batchNummerTxtField, tapningVBox);
+        informationBox.setSpacing(7.5);
+        pane.add(informationBox, 2, 0);
 
 
     }
