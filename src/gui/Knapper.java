8cc7c4e93e9a77259f26fc4a2e3b4ea888a178a1
Jonas-Petersen <jonasvm6@hotmail.com>
2025-05-08 12:27:10 +0200

    Jonas's bussing gui ændringer

---
+++ src/gui/Knapper.java
@@ -0,0 +1,63 @@
+package gui;
+
+import application.model.Fad;
+import gui.OpretVinduer.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class Knapper {
+    public static Button OpretFadButton() {
+        Button opretFadBtn = new Button("Opret Fad");
+        opretFadBtn.setOnMouseClicked(event -> {
+            OpretFadVindue fadVindue = new OpretFadVindue("Opret Fad");
+            fadVindue.show();
+        });
+        return opretFadBtn;
+    }
+
+    public static Button OpretDestillatButton() {
+        Button opretDestillatBtn = new Button("Opret Destillat");
+        opretDestillatBtn.setOnMouseClicked(event -> {
+            OpretDestillatVindue destillatVindue = new OpretDestillatVindue("Opret Destillat");
+            destillatVindue.show();
+        });
+        return opretDestillatBtn;
+    }
+
+    public static Button OpretVandButton() {
+        Button opretVandBtn = new Button("Opret Vand");
+        opretVandBtn.setOnMouseClicked(event -> {
+            OpretVandVindue vandVindue = new OpretVandVindue("Opret Råvare");
+            vandVindue.show();
+        });
+        return opretVandBtn;
+    }
+
+    public static Button OpretKornButton() {
+        Button opretKornBtn = new Button("Opret Korn");
+        opretKornBtn.setOnMouseClicked(event -> {
+            OpretKornVindue kornVindue = new OpretKornVindue("Opret Korn");
+            kornVindue.show();
+        });
+        return opretKornBtn;
+    }
+
+    public static Button OpretBatchButton() {
+        Button opretBatchBtn = new Button("Opret Batch");
+        opretBatchBtn.setOnMouseClicked(event -> {
+            OpretBatchVindue batchVindue = new OpretBatchVindue("Opret Batch");
+            batchVindue.show();
+        });
+        return opretBatchBtn;
+    }
+
+    public static Button TilføjDestillatButton(ListView<Fad> fadListView) {
+        Button tilføjDestillatBtn = new Button("Tilføj Destillat");
+
+        tilføjDestillatBtn.setOnMouseClicked(event -> {
+            TilføjDestillatVindue tilføjDestillatVindue = new TilføjDestillatVindue("Tilføj Destillat", fadListView.getSelectionModel().getSelectedItem());
+            tilføjDestillatVindue.show();
+        });
+        return tilføjDestillatBtn;
+    }
+}
