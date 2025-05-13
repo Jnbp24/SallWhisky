ca61d9d6e00f1914072871ba23cdea96da7414d0
Jonas-Petersen <jonasvm6@hotmail.com>
2025-05-13 10:14:33 +0200

    omhældning backend

--- src/gui/TilføjDestillatVindue.java
+++ src/gui/TilføjDestillatVindue.java
@@ -52,25 +52,16 @@ public class TilføjDestillatVindue extends Stage {
         pane.add(medarbejderListView, 2, 1);
         medarbejderListView.getItems().setAll(Controller.getMedarbejdere());
 
-        resterendeMængdeTextfield.setText(String.valueOf(fad.getFadStørrelse()));
-        mængdeFraDestillatTextfield.setText("0");
 
         Button tilføjBtn = new Button("Tilføj");
         tilføjBtn.setOnMouseClicked(event -> {
             resterendeMængdeTextfield.setText(String.valueOf(fad.getFadStørrelse()));
             mængdeFraDestillatTextfield.setText("0");
             try {
-                double resterendeMængde = Double.parseDouble(resterendeMængdeTextfield.getText());
-                double mængdeFraDestillat = Double.parseDouble(mængdeFraDestillatTextfield.getText());
-
-                if (mængdeFraDestillat > resterendeMængde) {
-                    throw new IllegalArgumentException("Mængde overskrider resterende volume");
-                }
-
                 påfyldning = Controller.tilføjDestillat(fad, destillatListView.getSelectionModel().getSelectedItem(), Double.parseDouble(mængdeFraDestillatTextfield.getText()));
                 double difference = Double.parseDouble(resterendeMængdeTextfield.getText()) - Double.parseDouble(mængdeFraDestillatTextfield.getText());
                 resterendeMængdeTextfield.setText(String.valueOf(difference));
-
+                mængdeFraDestillatTextfield.setText("0");
 
             } catch (IllegalArgumentException e) {
                 Alert fejlAlert = new Alert(Alert.AlertType.ERROR);
