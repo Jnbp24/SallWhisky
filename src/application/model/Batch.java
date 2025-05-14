057b992e6c4f458d083909ede50f8f6b8250c98c
Prog1Lektion02 <sara.statoua@gmail.com>
2025-05-14 13:46:14 +0200

    barch og storeage

--- src/application/model/Batch.java
+++ src/application/model/Batch.java
@@ -12,7 +12,7 @@ public class Batch {
     private Tapning tapning;
     private ArrayList<Flaske> flasker = new ArrayList<>();
 
-    public Batch(ArrayList<Kornsort> kornsorter, String fadtype, int batchNummer, String batchNavn, double fortyndelseLiter) {
+    public Batch(ArrayList<Kornsort> kornsorter, String fadtype, int batchNummer, String batchNavn, double fortyndelseLiter, Tapning tapning) {
         this.kornsorter = kornsorter;
         this.fadtype = fadtype;
         this.batchNummer = batchNummer;
