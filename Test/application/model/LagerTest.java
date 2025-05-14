bc49cb8d5a92f9de12041205e2dfeb9ac27ea61d
Jonas-Petersen <jonasvm6@hotmail.com>
2025-05-14 13:36:59 +0200

    div fix

--- Test/application/model/LagerTest.java
+++ Test/application/model/LagerTest.java
@@ -3,6 +3,8 @@ package application.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LagerTest {
@@ -11,7 +13,7 @@ class LagerTest {
     private Fad fad;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         lager = new Lager("Lager 2");
         lager.opretLagerplads("B", 2);
         lagerplads = lager.getPladser().getFirst();
@@ -31,13 +33,35 @@ class LagerTest {
     }
 
     @Test
-    void findFadPåFadNr() {
+    void findFad_PåFadNummer() {
         lagerplads.placerFad(fad);
         assertEquals(lagerplads, lager.findFadPåFadNummer(fad.getNummer()));
     }
 
     @Test
-    void findTapklar() {
+    void findFad_PåNmNummer() {
+        Lager lager = new Lager("Lager 1");
+        lager.opretLagerplads("A", 1);
+        lager.opretLagerplads("B", 1);
 
+        Fad fad1 = new Fad(1, "Eg", 30.0, 2);
+        Fad fad2 = new Fad(2, "Eg", 50.0, 3);
+        Kornsort kornsort = new Kornsort("korn", "kornmark", 30, Ristethed.IKKE_RISTET);
+        Vand vand = new Vand("vand", "vandmark", 10);
+
+        Destillat destillat1 = new Destillat("NM1", 63.5, kornsort, vand, true);
+        Destillat destillat2 = new Destillat("NM2", 63.5, kornsort, vand, true);
+
+        fad1.tilføjDestillat(destillat1);
+        fad2.tilføjDestillat(destillat2);
+
+        lager.getPladser().get(0).placerFad(fad1);
+        lager.getPladser().get(1).placerFad(fad2);
+
+        ArrayList<Lagerplads> resultat = lager.findFadPåNmNummer("NM1");
+
+        assertEquals(1, resultat.size(), "Bør tjekke at kun et fad findes med dette NM nummer");
+        assertEquals("A", resultat.get(0).getReol(), "Bør tjekke at NM nummeret ligger på den forventede reol plads");
+        assertTrue(resultat.get(0).getFad().getDestillater().contains(destillat1), "Bør tjekke at resultatet faktisk indeholder destillatet tilsvarende NM nummeret");
     }
 }
